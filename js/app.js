3942a4939c6f0d1b1304d682e9596a5c817ec5e7
Apungu Raymond <63607228+Raymond-ap@users.noreply.github.com>
2021-02-27 01:17:19 -0800

    updated theme changer with local storage

--- js/app.js
+++ js/app.js
@@ -2,17 +2,42 @@ const main = document.querySelector('.main')
 const head_section = document.querySelector('.head')
 const toggleTheme = document.querySelector('.toggle')
 const DOMBody = document.querySelector('.DOMBody')
+const theme = document.querySelectorAll('.theme')
 
 
+const getTheme = localStorage.getItem('currentTheme')
 
-toggleTheme.addEventListener('click', () => {
-    toggleTheme.classList.toggle('mode')
-    DOMBody.classList.toggle('theme-toggle')
-    window.localStorage.toggle = 'theme-toggle'
-})
-
-if (window.location.toggle == 'theme-toggle') {
-    
+if(getTheme == null) {
+    changeTheme('dark')
 } else {
-    
+    changeTheme(getTheme)
 }
+
+theme.forEach(elemet => {
+    elemet.addEventListener('click', e => {
+        let mode = elemet.dataset.mode
+        changeTheme(mode)
+    })
+});
+
+
+// Function to set theme
+function changeTheme(mode){
+    if (mode == 'light') {
+        DOMBody.classList.add('theme-toggle')
+    } else if(mode == 'dark') {
+        DOMBody.classList.remove('theme-toggle')
+    }
+
+    // Saving theme to local storage
+    localStorage.setItem('currentTheme', mode)
+}
+
+
+
+// toggleTheme.addEventListener('click', () => {
+//     toggleTheme.classList.toggle('mode')
+//     DOMBody.classList.toggle('theme-toggle')
+//     window.localStorage.toggle = 'theme-toggle'
+// })
+
