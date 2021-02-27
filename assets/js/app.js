ad6983d91848ef60dc09971494255d6462b94a23
Apungu Raymond <63607228+Raymond-ap@users.noreply.github.com>
2021-02-27 12:51:46 -0800

    commit

---
+++ assets/js/app.js
@@ -0,0 +1,43 @@
+const main = document.querySelector('.main')
+const head_section = document.querySelector('.head')
+const toggleTheme = document.querySelector('.toggle')
+const DOMBody = document.querySelector('.DOMBody')
+const theme = document.querySelectorAll('.theme')
+
+
+const getTheme = localStorage.getItem('currentTheme')
+
+if(getTheme == null) {
+    changeTheme('dark')
+} else {
+    changeTheme(getTheme)
+}
+
+theme.forEach(elemet => {
+    elemet.addEventListener('click', e => {
+        let mode = elemet.dataset.mode
+        changeTheme(mode)
+    })
+});
+
+
+// Function to set theme
+function changeTheme(mode){
+    if (mode == 'light') {
+        DOMBody.classList.add('theme-toggle')
+    } else if(mode == 'dark') {
+        DOMBody.classList.remove('theme-toggle')
+    }
+
+    // Saving theme to local storage
+    localStorage.setItem('currentTheme', mode)
+}
+
+
+
+// toggleTheme.addEventListener('click', () => {
+//     toggleTheme.classList.toggle('mode')
+//     DOMBody.classList.toggle('theme-toggle')
+//     window.localStorage.toggle = 'theme-toggle'
+// })
+
--- todo_application/settings.py
+++ todo_application/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'lx5*m=rql%25kk1tah7nl*to$#q7aexfgfvx#%ihl0sj9br3e*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['frontendchallengetodo.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
