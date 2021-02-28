78fb9d59eb41e38dde4dfba03e2f194103add84a
Apungu Raymond <63607228+Raymond-ap@users.noreply.github.com>
2021-02-28 01:55:55 -0800

    Update settings.py

--- todo_application/settings.py
+++ todo_application/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'lx5*m=rql%25kk1tah7nl*to$#q7aexfgfvx#%ihl0sj9br3e*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = Flase
+DEBUG = False
 
 ALLOWED_HOSTS = ['frontendchallengetodo.herokuapp.com']
 
